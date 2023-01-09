3492fbff2d30a24ae037cdeae2ce526853ab5c80
Rafael Souza <rafael.mycontact@gmail.com>
2023-01-09 16:21:20 -0300

    App Start
    Created the screens (activitys and fragments) and other parts of the ui layer

---
+++ app/src/main/java/br/com/rafael/medicineorganizer/ui/auth/RecoveryAccountFragment.kt
@@ -0,0 +1,27 @@
+package br.com.rafael.medicineorganizer.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.rafael.medicineorganizer.R
+import br.com.rafael.medicineorganizer.databinding.FragmentRecoveryAccountBinding
+
+class RecoveryAccountFragment : Fragment() {
+    private var _binding: FragmentRecoveryAccountBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecoveryAccountBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
