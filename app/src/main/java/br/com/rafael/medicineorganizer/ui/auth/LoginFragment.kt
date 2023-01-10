5c29e541e0a96cdec97698301080cbd73934b684
Rafael Souza <rafael.mycontact@gmail.com>
2023-01-10 10:59:07 -0300

    Created basic navigation of the app

--- app/src/main/java/br/com/rafael/medicineorganizer/ui/auth/LoginFragment.kt
+++ app/src/main/java/br/com/rafael/medicineorganizer/ui/auth/LoginFragment.kt
@@ -1,10 +1,13 @@
 package br.com.rafael.medicineorganizer.ui.auth
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.navigation.fragment.findNavController
 import br.com.rafael.medicineorganizer.R
 import br.com.rafael.medicineorganizer.databinding.FragmentLoginBinding
 
@@ -19,6 +22,29 @@ class LoginFragment : Fragment() {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClick()
+    }
+
+    private fun initClick() {
+        binding.btnCreateAccount.setOnClickListener {
+            binding.btnCreateAccount.apply {
+                setTextColor(resources.getColor(R.color.grey, null))
+                setBackgroundColor(resources.getColor(R.color.color_default, null))
+            }
+
+            findNavController().navigate(R.id.action_loginFragment2_to_registerFragment2)
+        }
+        binding.btnRecoveryAccount.setOnClickListener {
+            binding.btnRecoveryAccount.apply {
+                setBackgroundColor(resources.getColor(R.color.color_default, null))
+                setTextColor(resources.getColor(R.color.grey, null))
+            }
+            findNavController().navigate(R.id.action_loginFragment2_to_recoveryAccountFragment2)
+        }
     }
 
     override fun onDestroyView() {
